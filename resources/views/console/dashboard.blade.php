44c4fabbb37188368bde1b4695f2b6ccdf207637
ckwwingo <99191737+wingck@users.noreply.github.com>
2024-06-15 16:29:22 -0400

    Add files via upload

--- resources/views/console/dashboard.blade.php
+++ resources/views/console/dashboard.blade.php
@@ -7,8 +7,9 @@
     <ul id="dashboard">
         <li><a href="/console/projects/list">Manage Projects</a></li>
         <li><a href="/console/types/list">Manage Types</a></li>
-        <li><a href="/console/users/list">Manage Users</a></li>
         <li><a href="/console/entries/list">Manage Entries</a></li>
+        <li><a href="/console/topics/list">Manage Topics</a></li>
+        <li><a href="/console/users/list">Manage Users</a></li>
         <li><a href="/console/logout">Log Out</a></li>
     </ul>
 
